81ef8acf335d8235364f966341206153c3db67dc
Adam Fisk <afisk@getlantern.org>
2025-01-23 15:42:34 -0700

    Add port for smart dialer

--- race_transport.go
+++ race_transport.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"log/slog"
+	"net"
 	"net/http"
 	"sync/atomic"
 	"time"
@@ -21,6 +21,7 @@ func newRaceTransport(httpDialers ...httpDialer) http.RoundTripper {
 }
 
 func (t *raceTransport) RoundTrip(req *http.Request) (*http.Response, error) {
+	log.Debugf("Starting RoundTrip race %v", req)
 	// Try all methods in parallel and return the first successful response.
 	// If all fail, return the last error.
 	ctx, cancel := context.WithTimeout(req.Context(), 1*time.Minute)
@@ -31,17 +32,33 @@ func (t *raceTransport) RoundTrip(req *http.Request) (*http.Response, error) {
 	var httpErrors atomic.Int64
 	var roundTrippherCh = make(chan http.RoundTripper)
 	var errCh = make(chan error)
+	log.Debugf("Dialing with %v dialers", len(t.httpDialers))
 	for _, d := range t.httpDialers {
 		go func(d httpDialer) {
 			// We first create connected http.RoundTrippers prior to sending the request.
 			// With this method, we don't have to worry about the idempotency of the request
 			// because we ultimately try the connections serially in the next step.
-			connectedRoundTripper, err := d(ctx, req.URL.Host)
+			addr := req.URL.Host
+
+			// The smart dialer requires the port to be specified, so we add it if it's
+			// missing. We can't do this in the dialer itself because the scheme
+			// is stripped by the time the dialer is called.
+			if _, _, err := net.SplitHostPort(addr); err != nil {
+				if req.URL.Scheme == "https" {
+					addr = net.JoinHostPort(addr, "443")
+				} else {
+					addr = net.JoinHostPort(addr, "80")
+				}
+			}
+			log.Debugf("Dialing %v", addr)
+			connectedRoundTripper, err := d(ctx, addr)
 			if err != nil {
+				log.Errorf("Error dialing to %v:\n%v", addr, err)
 				if httpErrors.Add(1) == int64(len(t.httpDialers)) {
 					errCh <- fmt.Errorf("failed to connect to any dialer with last error: %v", err)
 				}
 			} else {
+				log.Debugf("Dialing done %v", addr)
 				roundTrippherCh <- connectedRoundTripper
 			}
 		}(d)
@@ -51,11 +68,13 @@ func (t *raceTransport) RoundTrip(req *http.Request) (*http.Response, error) {
 	for i := 0; i < retryTimes; i++ {
 		select {
 		case roundTripper := <-roundTrippherCh:
+			log.Debugf("Got connected roundTripper")
 			// If we get a connection, try to send the request.
 			resp, err := roundTripper.RoundTrip(req)
+			log.Debugf("Got response %v", resp)
 			// If the request fails, close the connection and return the error.
 			if err != nil {
-				slog.Error("HTTP request failed", "error", err)
+				log.Errorf("HTTP request failed %v", err)
 				continue
 			}
 
