47dbbd27f6aa4d470c83c3e3b0ad6d1b48d79100
Adam Fisk <afisk@getlantern.org>
2025-06-18 11:58:40 -0600

    Add request cloning to avoid any RoundTripper consuming POST bodies

--- race_transport.go
+++ race_transport.go
@@ -1,9 +1,11 @@
 package kindling
 
 import (
+	"bytes"
 	"context"
 	"errors"
 	"fmt"
+	"io"
 	"net"
 	"net/http"
 	"sync/atomic"
@@ -28,7 +30,6 @@ func newRaceTransport(panicListener func(string), roundTripperGenerators ...roun
 }
 
 func (t *raceTransport) RoundTrip(req *http.Request) (*http.Response, error) {
-	log.Debug("Starting RoundTrip race", "host", req.URL.Host)
 	// Try all methods in parallel and return the first successful response.
 	// If all fail, return the last error.
 	ctx, cancel := context.WithTimeout(req.Context(), 1*time.Minute)
@@ -68,17 +69,17 @@ func (t *raceTransport) RoundTrip(req *http.Request) (*http.Response, error) {
 	return nil, errors.New("failed to get response")
 }
 
-func (t *raceTransport) connectedRoundTripper(ctx context.Context, d roundTripperGenerator, req *http.Request, errCh chan error, responseChan chan *http.Response, httpErrors *atomic.Int64) {
+func (t *raceTransport) connectedRoundTripper(ctx context.Context, d roundTripperGenerator, originalReq *http.Request, errCh chan error, responseChan chan *http.Response, httpErrors *atomic.Int64) {
 	// We first create connected http.RoundTrippers prior to sending the request.
 	// With this method, we don't have to worry about the idempotency of the request
 	// because we ultimately try the connections serially in the next step.
-	addr := req.URL.Host
+	addr := originalReq.URL.Host
 
 	// The smart dialer requires the port to be specified, so we add it if it's
 	// missing. We can't do this in the dialer itself because the scheme
 	// is stripped by the time the dialer is called.
 	if _, _, err := net.SplitHostPort(addr); err != nil {
-		if req.URL.Scheme == "https" {
+		if originalReq.URL.Scheme == "https" {
 			addr = net.JoinHostPort(addr, "443")
 		} else {
 			addr = net.JoinHostPort(addr, "80")
@@ -89,19 +90,17 @@ func (t *raceTransport) connectedRoundTripper(ctx context.Context, d roundTrippe
 			errCh <- fmt.Errorf("%v failed to connect to any dialer with last error: %v", d.name(), err)
 		}
 	}
-	log.Debug("Dialing", "addr", addr)
 	connectedRoundTripper, err := d.roundTripper(ctx, addr)
 	if err != nil {
 		errFunc(err)
 	} else {
-		log.Debug("Dialing done", "addr", addr)
-		log.Debug("Got connected roundTripper", "host", req.URL.Host)
 		if ctx.Err() != nil {
 			// context is canceled - we should not proceed with the request
-			log.Debug("Context canceled before sending request", "host", req.URL.Host)
+			log.Debug("Context canceled before sending request", "host", originalReq.URL.Host)
 			errFunc(ctx.Err())
 			return
 		}
+		req := cloneRequest(originalReq)
 		// If we get a connection, try to send the request.
 		resp, err := connectedRoundTripper.RoundTrip(req)
 		if err != nil {
@@ -119,4 +118,28 @@ func (t *raceTransport) connectedRoundTripper(ctx context.Context, d roundTrippe
 			}
 		}
 	}
+}
+
+// cloneRequest creates a copy of the provided HTTP request, including its body.
+// If the body is nil or http.NoBody, it simply returns a clone without reading the body.
+// This is important because, since we're racing requests, it's possible that the body
+// has been consumed by a previous request.
+func cloneRequest(req *http.Request) *http.Request {
+	clonedReq := req.Clone(req.Context())
+	if req.Body == http.NoBody || req.Body == nil {
+		// If the request body is nil, we can just return a clone without reading it.
+		return clonedReq
+	}
+	// Read the original body into a buffer
+	bodyBytes, err := io.ReadAll(req.Body)
+	if err != nil {
+		log.Error("Error reading body:", "error", err)
+		return req
+	}
+	req.Body.Close() // Close the original body
+
+	// Replace the bodies with new readers from the buffer
+	req.Body = io.NopCloser(bytes.NewReader(bodyBytes))
+	clonedReq.Body = io.NopCloser(bytes.NewReader(bodyBytes))
+	return clonedReq
 }
